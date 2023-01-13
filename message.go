b0dd0ec7448cbcc005dcb36379620db075bd0b9e
elmarsan <mselias97@gmail.com>
2023-01-13 21:57:44 +0100

    validate msg command and net

--- message.go
+++ message.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
 // Last protocol version 70015
 
 const (
@@ -13,12 +19,41 @@ const MsgHeaderSize = 24
 const CommandSize = 12
 
 type MsgHeader struct {
-	net      uint32 // 4 bytes
-	command  uint16 // 12 bytes
-	length   uint32 // 4 bytes
-	checksum uint32 // 4 bytes
+	net      uint32
+	command  uint64
+	length   uint32
+	checksum uint32
 }
 
 const (
-	Version = 0x76657273696F6E0000000000
+	Version uint64 = 0x76657273696F6E00
 )
+
+func MsgHeaderFromRaw(raw []byte) (*MsgHeader, error) {
+	var net uint32
+	err := binary.Read(bytes.NewReader(raw), binary.LittleEndian, &net)
+	if err != nil {
+		return nil, fmt.Errorf("%s", "Error reading msg net")
+	}
+
+	if net != mainnet && net != testnet && net != testnet3 {
+		return nil, fmt.Errorf("%s", "Invalid net")
+	}
+
+	var command uint64
+	err = binary.Read(bytes.NewReader(raw[4:16]), binary.BigEndian, &command)
+	if err != nil {
+		return nil, fmt.Errorf("%s", "Error reading msg command")
+	}
+
+	if command != Version {
+		return nil, fmt.Errorf("Invalid command 0x%x", command)
+	}
+
+	return &MsgHeader{
+		net:      net,
+		command:  command,
+		length:   0x00,
+		checksum: 0x00,
+	}, nil
+}
