0bd919e40ac8e98b9a8c9fc62c5abb06f79b5327
elmarsan <mselias97@gmail.com>
2023-01-17 22:44:15 +0100

    add msg_version fields decoding

--- msg_version.go
+++ msg_version.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
 	"net"
 	"time"
 )
@@ -19,9 +20,9 @@ type MsgVersion struct {
 	Timestamp time.Time
 	// AddRecv represents the network address of the node receiving this msg.
 	// AddRecv [26]byte
-	AddRecv net.Addr
+	AddRecv net.IP
 	// AddFrom represents the network address of the node sending this msg.
-	AddFrom net.Addr
+	// AddFrom net.Addr
 	// Nonce represents random nonce generated every time a version msg is sent.
 	Nonce uint64
 	// UserAgent represents information of the node.
@@ -42,16 +43,34 @@ func (msgv *MsgVersion) Encode(b *bytes.Buffer) error {
 // TODO: implement
 func (msgv *MsgVersion) Decode(b *bytes.Buffer) error {
 	headers := &MsgHeader{}
-	headers.Decode(b)
+	err := headers.Decode(b)
+	if err != nil {
+		return err
+	}
 
 	version := binary.LittleEndian.Uint32(b.Bytes()[24:28])
 	services := binary.LittleEndian.Uint64(b.Bytes()[28:36])
 	unix := binary.LittleEndian.Uint64(b.Bytes()[36:44])
+	addrRecv := b.Bytes()[52:70]
+	nonce := binary.LittleEndian.Uint64(b.Bytes()[96:104])
+	relay := b.Bytes()[b.Len()-1:][0]
+	startHeight := binary.LittleEndian.Uint32(b.Bytes()[b.Len()-5 : b.Len()-1])
 
 	msgv.Headers = *headers
 	msgv.Version = version
 	msgv.Services = services
 	msgv.Timestamp = time.Unix(int64(unix), 0)
+	msgv.AddRecv = net.IP(addrRecv[:net.IPv6len])
+	msgv.Nonce = nonce
+	msgv.StartHeight = startHeight
+
+	if relay == 0x00 {
+		msgv.Relay = false
+	} else {
+		msgv.Relay = true
+	}
+
+	fmt.Printf("Start height 0x%x\n", msgv.StartHeight)
 
 	return nil
 }
