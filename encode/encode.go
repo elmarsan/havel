f160a5624e1c97b4a01f4620a229d0f653585fdf
elmarsan <mselias97@gmail.com>
2023-01-21 18:22:26 +0100

    add encoding methods

--- encode/encode.go
+++ encode/encode.go
@@ -1,84 +1,166 @@
 package encode
 
 import (
-	"bytes"
 	"encoding/binary"
+	"io"
 )
 
-// readUint8 reads uint8 from b into d.
-func readUint8(b *bytes.Buffer, order binary.ByteOrder, d *uint8) error {
-	if err := binary.Read(b, order, d); err != nil {
+// readUint8 reads uint8 from r into d.
+func readUint8(r io.Reader, order binary.ByteOrder, d *uint8) error {
+	if err := binary.Read(r, order, d); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-// readUint16 reads uint16 from b into d.
-func readUint16(b *bytes.Buffer, order binary.ByteOrder, d *uint16) error {
-	if err := binary.Read(b, order, d); err != nil {
+// readUint16 reads uint16 from r into d.
+func readUint16(r io.Reader, order binary.ByteOrder, d *uint16) error {
+	if err := binary.Read(r, order, d); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-// readUint32 reads uint32 from b into d.
-func readUint32(b *bytes.Buffer, order binary.ByteOrder, d *uint32) error {
-	if err := binary.Read(b, order, d); err != nil {
+// readUint32 reads uint32 from r into d.
+func readUint32(r io.Reader, order binary.ByteOrder, d *uint32) error {
+	if err := binary.Read(r, order, d); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-// readUint64 reads uint64 from b into d.
-func readUint64(b *bytes.Buffer, order binary.ByteOrder, d *uint64) error {
-	if err := binary.Read(b, order, d); err != nil {
+// readUint64 reads uint64 from r into d.
+func readUint64(r io.Reader, order binary.ByteOrder, d *uint64) error {
+	if err := binary.Read(r, order, d); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-// readSlice reads uint64 from b into d.
-func readSlice(b *bytes.Buffer, order binary.ByteOrder, d *[]byte) error {
-	if err := binary.Read(b, order, d); err != nil {
+// readSlice reads slice from r into d.
+func readSlice(r io.Reader, order binary.ByteOrder, d *[]byte) error {
+	if err := binary.Read(r, order, d); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-// decode decodes any type from b into d.
-func decode(b *bytes.Buffer, order binary.ByteOrder, d interface{}) error {
+// decode decodes r into d.
+func decode(r io.Reader, order binary.ByteOrder, d interface{}) error {
 	switch t := d.(type) {
 	case *uint8:
-		return readUint8(b, order, t)
+		return readUint8(r, order, t)
 	case *uint16:
-		return readUint16(b, order, t)
+		return readUint16(r, order, t)
 	case *uint32:
-		return readUint32(b, order, t)
+		return readUint32(r, order, t)
 	case *uint64:
-		return readUint64(b, order, t)
+		return readUint64(r, order, t)
 	case *[]byte:
-		return readSlice(b, order, t)
+		return readSlice(r, order, t)
 	}
 	return nil
 }
 
 // DecodeVal holds target value for decoding.
 type DecodeVal struct {
-	// order specifies encoding byte order
-	order binary.ByteOrder
-	// d holds data of val
-	d interface{}
+	// Order specifies encoding byte Order.
+	Order binary.ByteOrder
+	// Val holds data of val.
+	Val interface{}
 }
 
-// DecodeBatch decodes batch of values from b into DecodeVal.d
-func DecodeBatch(b *bytes.Buffer, vals ...DecodeVal) error {
+// DecodeBatch decodes batch of values from b into DecodeVal.Val.
+func DecodeBatch(r io.Reader, vals ...DecodeVal) error {
 	for _, val := range vals {
-		if err := decode(b, val.order, val.d); err != nil {
+		if err := decode(r, val.Order, val.Val); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// writeUint8 writes uint8 from w into d.
+func writeUint8(w io.Writer, order binary.ByteOrder, d *uint8) error {
+	if err := binary.Write(w, order, d); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// writeUint16 writes uint16 from w into d.
+func writeUint16(w io.Writer, order binary.ByteOrder, d *uint16) error {
+	if err := binary.Write(w, order, d); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// writeUint32 writes uint32 from w into d.
+func writeUint32(w io.Writer, order binary.ByteOrder, d *uint32) error {
+	if err := binary.Write(w, order, d); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// writeUint64 writes uint64 from w into d.
+func writeUint64(w io.Writer, order binary.ByteOrder, d *uint64) error {
+	if err := binary.Write(w, order, d); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// writeSlice writes slice from w into d.
+func writeSlice(w io.Writer, order binary.ByteOrder, d *[]byte) error {
+	if err := binary.Write(w, order, d); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// encode encodes d into w.
+func encode(w io.Writer, order binary.ByteOrder, d interface{}) error {
+	switch t := d.(type) {
+	case *uint8:
+		return writeUint8(w, order, t)
+	case *uint16:
+		return writeUint16(w, order, t)
+	case *uint32:
+		return writeUint32(w, order, t)
+	case *uint64:
+		return writeUint64(w, order, t)
+	case *[]byte:
+		return writeSlice(w, order, t)
+	}
+
+	return nil
+}
+
+// EncodeVal holds target value for encoding.
+type EncodeVal struct {
+	// Order specifies decoding byte Order.
+	Order binary.ByteOrder
+	// Val holds data of val.
+	Val interface{}
+}
+
+// EncodeBatch encodes batch of values from EncodeVal.Val into w.
+func EncodeBatch(w io.Writer, vals ...EncodeVal) error {
+	for _, val := range vals {
+		if err := encode(w, val.Order, val.Val); err != nil {
 			return err
 		}
 	}
--- encode/encode_test.go
+++ encode/encode_test.go
@@ -14,7 +14,7 @@ func TestReadUint8(t *testing.T) {
 		var val uint8
 		err := readUint8(b, binary.LittleEndian, &val)
 		if err != nil {
-			t.Error("Could not read uint32")
+			t.Error("Could not read uint8")
 		}
 
 		if val != 0xaa {
@@ -173,20 +173,20 @@ func TestDecodeBatch(t *testing.T) {
 
 		vals := []DecodeVal{
 			{
-				order: binary.LittleEndian,
-				d:     &byteSliceVal,
+				Order: binary.LittleEndian,
+				Val:   &byteSliceVal,
 			},
 			{
-				order: binary.BigEndian,
-				d:     &uint16val,
+				Order: binary.BigEndian,
+				Val:   &uint16val,
 			},
 			{
-				order: binary.LittleEndian,
-				d:     &uint8SliceVal,
+				Order: binary.LittleEndian,
+				Val:   &uint8SliceVal,
 			},
 			{
-				order: binary.BigEndian,
-				d:     &uint32val,
+				Order: binary.BigEndian,
+				Val:   &uint32val,
 			},
 		}
 
@@ -209,6 +209,229 @@ func TestDecodeBatch(t *testing.T) {
 
 		if uint32val != expectedUint32val {
 			t.Errorf("Wrong uint32 decoding: actual (0x%x), expected (0x%x)", uint32val, expectedUint32val)
+		}
+	})
+}
+
+func TestWriteUint8(t *testing.T) {
+	d := []byte{}
+	b := bytes.NewBuffer(d)
+
+	t.Run("should write uint8", func(t *testing.T) {
+		var val uint8 = 0xaa
+		err := writeUint8(b, binary.LittleEndian, &val)
+		if err != nil {
+			t.Error("Could not write uint8")
+		}
+
+		if b.Bytes()[0] != val {
+			t.Errorf("Wrong uint8 write: actual (0x%x), expected (0xaa)", val)
+		}
+	})
+}
+
+func TestWriteUint16(t *testing.T) {
+	t.Run("should write uint16 (LittleEndian)", func(t *testing.T) {
+		d := []byte{}
+		b := bytes.NewBuffer(d)
+		var val uint16 = 0xaabb
+
+		err := writeUint16(b, binary.LittleEndian, &val)
+		if err != nil {
+			t.Error("Could not write uint16")
+		}
+
+		expected := [2]byte{
+			uint8(val),
+			uint8(val >> 8),
+		}
+
+		for i, b := range b.Bytes() {
+			if expected[i] != b {
+				t.Errorf("Wrong b[%d] write: actual (0x%x), expected (0x%x)", i, b, expected[i])
+			}
+		}
+	})
+
+	t.Run("should write uint16 (BigEndian)", func(t *testing.T) {
+		d := []byte{}
+		b := bytes.NewBuffer(d)
+		var val uint16 = 0xaabb
+
+		err := writeUint16(b, binary.BigEndian, &val)
+		if err != nil {
+			t.Error("Could not write uint16")
+		}
+
+		expected := [2]byte{
+			uint8(val >> 8),
+			uint8(val),
+		}
+
+		for i, b := range b.Bytes() {
+			if expected[i] != b {
+				t.Errorf("Wrong b[%d] write: actual (0x%x), expected (0x%x)", i, b, expected[i])
+			}
+		}
+	})
+}
+
+func TestWriteUint32(t *testing.T) {
+	t.Run("should write uint32 (LittleEndian)", func(t *testing.T) {
+		d := []byte{}
+		b := bytes.NewBuffer(d)
+		var val uint32 = 0xaabbccdd
+
+		err := writeUint32(b, binary.LittleEndian, &val)
+		if err != nil {
+			t.Error("Could not write uint32")
+		}
+
+		expected := [4]byte{
+			uint8(val),
+			uint8(val >> 8),
+			uint8(val >> 16),
+			uint8(val >> 24),
+		}
+
+		for i, b := range b.Bytes() {
+			if expected[i] != b {
+				t.Errorf("Wrong b[%d] write: actual (0x%x), expected (0x%x)", i, b, expected[i])
+			}
+		}
+	})
+
+	t.Run("should write uint32 (BigEndian)", func(t *testing.T) {
+		d := []byte{}
+		b := bytes.NewBuffer(d)
+		var val uint32 = 0xaabbccdd
+
+		err := writeUint32(b, binary.BigEndian, &val)
+		if err != nil {
+			t.Error("Could not write uint32")
+		}
+
+		expected := [4]byte{
+			uint8(val >> 24),
+			uint8(val >> 16),
+			uint8(val >> 8),
+			uint8(val),
+		}
+
+		for i, b := range b.Bytes() {
+			if expected[i] != b {
+				t.Errorf("Wrong b[%d] write: actual (0x%x), expected (0x%x)", i, b, expected[i])
+			}
+		}
+	})
+}
+
+func TestWriteUint64(t *testing.T) {
+	t.Run("should write uint64 (LittleEndian)", func(t *testing.T) {
+		d := []byte{}
+		b := bytes.NewBuffer(d)
+		var val uint64 = 0xaabbccdd11223344
+
+		err := writeUint64(b, binary.LittleEndian, &val)
+		if err != nil {
+			t.Error("Could not write uint64")
+		}
+
+		expected := [8]byte{
+			uint8(val),
+			uint8(val >> 8),
+			uint8(val >> 16),
+			uint8(val >> 24),
+			uint8(val >> 32),
+			uint8(val >> 40),
+			uint8(val >> 48),
+			uint8(val >> 56),
+		}
+
+		for i, b := range b.Bytes() {
+			if expected[i] != b {
+				t.Errorf("Wrong b[%d] write: actual (0x%x), expected (0x%x)", i, b, expected[i])
+			}
+		}
+	})
+
+	t.Run("should write uint64 (BigEndian)", func(t *testing.T) {
+		d := []byte{}
+		b := bytes.NewBuffer(d)
+		var val uint64 = 0xaabbccdd11223344
+
+		err := writeUint64(b, binary.BigEndian, &val)
+		if err != nil {
+			t.Error("Could not write uint64")
+		}
+
+		expected := [8]byte{
+			uint8(val >> 56),
+			uint8(val >> 48),
+			uint8(val >> 40),
+			uint8(val >> 32),
+			uint8(val >> 24),
+			uint8(val >> 16),
+			uint8(val >> 8),
+			uint8(val),
+		}
+
+		for i, b := range b.Bytes() {
+			if expected[i] != b {
+				t.Errorf("Wrong b[%d] write: actual (0x%x), expected (0x%x)", i, b, expected[i])
+			}
+		}
+	})
+}
+
+func TestEncodeBatch(t *testing.T) {
+	d := []byte{}
+	b := bytes.NewBuffer(d)
+
+	t.Run("should encode batch", func(t *testing.T) {
+		var uint32Val uint32 = 0xafabff6e
+		var uint64Val uint64 = 0x00000506ffbb
+		slice := []byte{0xaa, 0x8b, 0x9c}
+
+		vals := []EncodeVal{
+			{
+				Order: binary.LittleEndian,
+				Val:   &uint32Val,
+			},
+			{
+				Order: binary.BigEndian,
+				Val:   &uint64Val,
+			},
+			{
+				Order: binary.LittleEndian,
+				Val:   &slice,
+			},
+		}
+
+		err := EncodeBatch(b, vals...)
+		if err != nil {
+			t.Error("Could not encode")
+		}
+
+		expected := []byte{
+			uint8(uint32Val),
+			uint8(uint32Val >> 8),
+			uint8(uint32Val >> 16),
+			uint8(uint32Val >> 24),
+			uint8(uint64Val >> 56),
+			uint8(uint64Val >> 48),
+			uint8(uint64Val >> 40),
+			uint8(uint64Val >> 32),
+			uint8(uint64Val >> 24),
+			uint8(uint64Val >> 16),
+			uint8(uint64Val >> 8),
+			uint8(uint64Val),
+		}
+
+		expected = append(expected, slice...)
+
+		if bytes.Compare(b.Bytes(), expected) != 0 {
+			t.Error("Wrong encoding")
 		}
 	})
 }
